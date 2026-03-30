c551b42682e6c42a1bd26620cd9aeb242dc29fde
Tshireletso Seqeta <seqeta@gmail.com>
2026-03-30 21:52:11 +0200

    Add project files.

---
+++ MyApp/Controllers/ItemsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyApp.Models;
+
+namespace MyApp.Controllers
+{
+    public class ItemsController : Controller
+    {
+        public IActionResult Overview()
+        {
+            var item = new Item();
+            return View();
+        }
+    }
+}
---
+++ MyApp/Models/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace MyApp.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    
+}
